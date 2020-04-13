1abb9568e18a93562bfb9b14d50396e14e260db8
saherla <saherla03@gmail.com>
2020-04-13 16:55:14 -0500

    Fixed error redeclared v0.0.3

--- smsVerify.go
+++ smsVerify.go
@@ -8,54 +8,6 @@ import (
 	"strings"
 )
 
-// VerifySMSResponse Twilio Verify API response structure
-// See more: https://www.twilio.com/docs/verify/api/verification
-type VerifySMSResponse struct {
-	AccountSid       string            `json:"account_sid"`
-	Amount           string            `json:"amount,omitempty"`
-	Channel          string            `json:"channel"`
-	DateCreated      string            `json:"date_created"`
-	DateUpdated      string            `json:"date_updated"`
-	Lookup           Lookup            `json:"lookup"`
-	Payee            string            `json:"payee"`
-	SendCodeAttempts []SendCodeAttempt `json:"send_code_attempts"`
-	ServiceSid       string            `json:"service_sid"`
-	Sid              string            `json:"sid"`
-	Status           string            `json:"status"`
-	To               string            `json:"to"`
-	URL              string            `json:"url"`
-	Valid            bool              `json:"valid"`
-}
-
-// Lookup Twilio Verify API response structure
-type Lookup struct {
-	Carrier Carrier `json:"carrier"`
-}
-
-// Carrier Twilio Verify API response structure
-type Carrier struct {
-	ErrorCode         int    `json:"error_code,omitempty"`
-	MobileCountryCode string `json:"mobile_country_code"`
-	MobileNetworkCode string `json:"mobile_network_code"`
-	Name              string `json:"name"`
-	Type              string `json:"type"`
-}
-
-// SendCodeAttempt Twilio Verify API response structure
-type SendCodeAttempt struct {
-	Channel string `json:"channel"`
-	Time    string `json:"time"`
-}
-
-// Exception Twilio Verify API errors structure
-// See more: https://www.twilio.com/docs/usage/troubleshooting/debugging-your-application
-type Exception struct {
-	Code     int64  `json:"code"`
-	Message  string `json:"message"`
-	MoreInfo string `json:"more_info"`
-	Status   int64  `json:"status"`
-}
-
 // VerifySendSMS The function of sending SMS receives the recipient's number as a parameter.
 func (t *TwilioClient) VerifySendSMS(to string) (smsResp *VerifySMSResponse, exception *Exception, err error) {
 	msgData := url.Values{}
